ffc6613f3f0ce2a07f21d64f78de5b59e249ace1
PapiSops <116013967+PapiSops@users.noreply.github.com>
2024-04-30 20:29:49 -0600

    git modif

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,16 +1,34 @@
+# terraform {
+#   backend "s3" {
+#     bucket         = "my-ews-baket1"
+#     region         = "us-east-1"
+#     key            = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/EKS-TF/terraform.tfstate"
+#     dynamodb_table = "Lock-Files"
+#     encrypt        = true
+#   }
+#   required_version = ">=0.13.0"
+#   required_providers {
+#     aws = {
+#       version = ">= 2.7.0"
+#       source  = "hashicorp/aws"
+#     }
+#   }
+# } 
+
 terraform {
-  backend "s3" {
-    bucket         = "my-ews-baket1"
-    region         = "us-east-1"
-    key            = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/EKS-TF/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
+ backend "s3" {
+    bucket = "my-terraform-state-ghm"
+    key    = "prod/aws_infra"
+    region = "us-east-1"
+
+    dynamodb_table = "terraform-locks"
     encrypt        = true
-  }
   required_version = ">=0.13.0"
   required_providers {
     aws = {
       version = ">= 2.7.0"
       source  = "hashicorp/aws"
-    }
-  }
+      }
+   }
+  } 
 }
